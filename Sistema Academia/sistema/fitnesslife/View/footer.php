b0d2c2dfae1ff5f9b7868c0abdf910c527bee30b
Letícia de Souza <42966141+leticia-s@users.noreply.github.com>
2019-07-03 15:54:56 -0300

    esqueci disso

--- Sistema Academia/sistema/fitnesslife/View/footer.php
+++ Sistema Academia/sistema/fitnesslife/View/footer.php
@@ -2,7 +2,7 @@
 <footer>
     <div class="row">
         <div class="col-lg-12">
-            <p style="color: blue; font-weight: bold;"> Fitness Life  &nbsp;&nbsp; &copy; 2015</p>
+            <p style="color: blue; font-weight: bold;"> Fitness Life  &nbsp;&nbsp; &copy; 2019</p>
         </div>
     </div>
 </footer>
